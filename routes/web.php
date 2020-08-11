351e76e1d9633c1f70b21f5dba6f18292b7b676a
bagustasuru97 <bagustasuru97@gmail.com>
2020-08-11 19:11:18 +0700

    kembali

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/v-1', function () {
-    return view('welcome');
-});
-
-test;
